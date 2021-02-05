2cb93c357d9d6727a594f26e9ed4f4f6fb63429a
AnnaliinaLaamanen <annaliina.laamanen@metropolia.fi>
2021-02-05 12:41:28 +0200

    Lisäilyjä

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php include 'header.php'; ?>
+    
     <h1>Tervetuloa!</h1>
+    <br>
+    <p>Lorem ipsum dolor sit amet, pri liber dictas propriae ei, an laudem vivendo electram est. At est purto rebum primis. Quot postea eu qui, ius praesent aliquando dissentiet ne. Ne quo purto persius maiestatis,<br>his periculis repudiare intellegebat eu, sit et iudico noster pericula. Deserunt complectitur est at, id per dicit dolores, vis et dictas option labitur. Sit eu alterum officiis consectetuer.
+
+        Ad cum munere partem volutpat, augue veritus ancillae ne eam, ea vim labitur mediocrem suscipiantur. Per no sale patrioque mediocritatem. Ad cum dico albucius petentium, in viderer expetendis incorrupte pro, pri in oblique <br> laoreet iudicabit. Sea no dolores scriptorem, possit mnesarchum id vel. Viderer fastidii vim id, eu atomorum intellegam eam, veri delenit ceteros vel ad.
+
+        Ut libris adipisci mei, quem justo euripidis has ne, vix soluta dictas iudicabit te. In qui unum eius. Pertinacia dissentiunt est cu, has purto delicatissimi ex, rebum labore aliquando et pro. Munere option ad usu, graeci verear democritum nam in.</p>
+
 <?php include 'footer.php'; ?>
 
